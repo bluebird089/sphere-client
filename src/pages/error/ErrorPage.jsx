45a09f008329ad8060602f755732f0e306eaefbd
Blue Bird <bluebird089@proton.me>
2023-10-25 13:08:24 +0600

    updated error page

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div className="flex gap-1 flex-col h-screen w-full justify-center items-center">
+        <div className="flex gap-2 flex-col h-screen w-full justify-center items-center">
             <h3 className="font-bold text-6xl">404</h3>
             <h3 className="font-semibold text-4xl">Not Found</h3>
-            <Link to='/' className="btn">Back to Home</Link>
+            <Link to='/' className="btn bg-gray-900 hover:bg-black text-white rounded-full">Back to Home</Link>
         </div>
     );
 };
